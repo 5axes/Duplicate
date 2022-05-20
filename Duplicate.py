79f7dd20d9d0584032a0c842aafbe6b530eb1599
5axes <5axes@users.noreply.github.com>
2022-05-20 19:37:51 +0200

    Update Duplicate.py

--- Duplicate.py
+++ Duplicate.py
@@ -55,12 +55,10 @@ class Duplicate(Extension):
 
         Logger.log('d', "Info CuraVersion --> " + str(CuraVersion))
         
-        # Test version for futur release 4.9
-        if "master" in CuraVersion or "beta" in CuraVersion or "BETA" in CuraVersion:
-            # Master is always a developement version.
+        # Test version for Cura Master Release
+        if "master" in CuraVersion :
             self.Major=4
-            self.Minor=9
-            
+            self.Minor=20  
         else:
             try:
                 self.Major = int(CuraVersion.split(".")[0])
